b58245a86483823ed9b584df394fff8864fc65ec
Marcel Gruenauer <hanekomu@gmail.com>
2010-02-08 14:45:19 +0100

    initial commit

---
+++ lib/App/perlzonji.pm
@@ -0,0 +1,55 @@
+package App::perlzonji;
+use 5.008;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+1;
+__END__
+
+=head1 NAME
+
+App::perlzonji - a more knowledgeable perldoc
+
+=head1 SYNOPSIS
+
+    # perlzonji UNIVERSAL::isa
+    # (runs `perldoc perlobj`)
+
+=head1 DESCRIPTION
+
+This is just a marker package so you can install this distribution from the
+CPAN shell.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see L<http://search.cpan.org/dist/App-perlzonji/>.
+
+The development version lives at L<http://github.com/hanekomu/app-perlzonji/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
+
+=head1 AUTHORS
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Marcel GrE<uuml>nauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
